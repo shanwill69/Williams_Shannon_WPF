1dc6a8c41c29c11e86c714bdeebf70ac21201c04
Shannon <shanwill69@fullsail.com>
2014-07-17 10:21:46 -0500

    Logan's toys
    created favorite toy list and how much he played with each

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,3 +1,12 @@
-/**
- * Created by shannonwilliams on 7/16/14.
- */
+//what are Logan's favorite toys and which does he play with most
+
+var faveToy = ["beyBlades" , "matchBox" , "nerfGuns"];
+var beyBlades = (3 + 2+ 4 + 1)/4;  //average per week
+var matchBox = (4 + 1 + 2 + 3)/4;
+var nerfGuns = (6 + 1 + 1 + 4)/4;
+
+// after averaging playtime, nerfGuns win
+
+var winner = faveToy[2];
+
+console.log(winner);
