97edd9523a4cc2f91848531ed73de9dbc0ff1a10
Shannon <shanwill69@fullsail.com>
2014-07-23 15:18:05 -0500

    Created else/if
    made industry statements using else.if

--- Conditionals_Inustry/js/script.js
+++ Conditionals_Inustry/js/script.js
@@ -1,3 +1,18 @@
 //Shannon Williams July 23, 2014 Conditionals
 
-//
+//I will work for myself if I make good enough grades
+
+var goodGrades = 72;
+var averageGrades = 79;
+var badGrades = 60;
+
+//if I make outstanding grades, I will work for a big company
+if(goodGrades > averageGrades){
+    console.log("Microsoft, here I come!");
+//if I do "okay", then I will work for myself on the side
+}else if(goodGrades > badGrades){
+    console.log("Start my own company");
+//if I fail miserably...yikes!
+}else{
+    console.log ("Do you want fries with that?");
+}
