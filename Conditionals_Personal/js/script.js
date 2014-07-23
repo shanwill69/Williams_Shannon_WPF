9f0b4cf99eeecc17e220bfb8b721d6036c422f0e
Shannon <shanwill69@fullsail.com>
2014-07-23 14:35:16 -0500

    Wrote code for grades
    used a ternary instead of if/else

---
+++ Conditionals_Personal/js/script.js
@@ -0,0 +1,13 @@
+//Will my son get a reward for his grades?
+
+var testScore = 91; //if he gets good grades he will get a reward
+var reward;
+
+//if his grade is greater than or equal to 90, he can shop
+reward = (testScore >= 90) ? "Shop for a toy!" : "Study more!";
+console.log(reward);
+
+
+
+
+
