fc76040726f37aee3bdc4f99eda4f4c930708500
Shannon <shanwill69@fullsail.com>
2014-07-23 12:49:49 -0500

    Wrote a conditional statement
    Wrote code for weight loss or weight gain

---
+++ Expressions_Personal 2/js/script.js
@@ -0,0 +1,10 @@
+//what is my average weekday TV viewing
+
+var monday = 4; //hours per day
+var tuesday = 3;
+var wednesday = 5;
+var thursday = 2;
+var average = (monday + tuesday + wednesday + thursday)/4; //add days divide by number of days
+
+
+
