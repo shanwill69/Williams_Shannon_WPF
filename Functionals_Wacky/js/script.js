be91755053fd27ff6b4d7030f9c223400cb862d0
Shannon <shanwill69@fullsail.com>
2014-07-29 13:25:11 -0500

    Functions Wacky
    trying to create a formula for milk to cookie ratio

---
+++ Functionals_Wacky/js/script.js
@@ -0,0 +1,12 @@
+//Shannon Williams Functionals Wacky July 28, 2014
+
+// How much milk do I need for my cookies?
+
+function gotMilk(milk){
+    var cookies = 2;
+    var cupMilk = milk * cookies;
+    console.log(cupMilk);
+    if(milk < 2);
+    console.log("I will need " + cupMilk + " ounces of milk for every two cookies");
+}
+gotMilk(2);
