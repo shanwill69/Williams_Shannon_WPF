268524a19727e21d9f1984b29149ccf43ebc4dc0
Shannon <shanwill69@fullsail.com>
2014-07-29 13:42:26 -0500

    functionals Industry
    created code for lines of code per minute

---
+++ Functionals_Industry/js/script.js
@@ -0,0 +1,13 @@
+//Shannon Williams Functionals Industry July 28, 2014
+
+
+// how many lines of code can I write in one hour
+
+function writeCode(linesOfCode) {
+
+    var perMinute = linesOfCode * 60;
+    console.log("I can write " + perMinute + " lines of code per minute");
+
+}
+
+writeCode(4);
--- Functionals_Wacky/js/script.js
+++ Functionals_Wacky/js/script.js
@@ -3,12 +3,12 @@
 // How much milk do I need for my cookies?
 
 function gotMilk(){
-    var milk = 4;
-    var cookies = 1;
-    var cupMilk = milk * cookies;
+    var milk = 4; //I need 2 ounces of milk for every two cookies
+    var cookies = 1;  //how many cookies I have
+    var cupMilk = milk * cookies; //ounces of milk times amount of cookies
     if(milk < 2){
         console.log("You don't have enough milk for your cookies")
-
+        //if I don't have enough milk, I can't have cookies
     }
     if(milk >= 2){
         console.log("Nom nom nom")
