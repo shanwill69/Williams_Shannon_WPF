91e8ec323012ddfb2985d2aa56e02c7c98a298d9
Shannon <shanwill69@fullsail.com>
2014-07-29 13:30:48 -0500

    functional wacky
    created if else statements

--- Functionals_Wacky/js/script.js
+++ Functionals_Wacky/js/script.js
@@ -2,11 +2,17 @@
 
 // How much milk do I need for my cookies?
 
-function gotMilk(milk){
-    var cookies = 2;
+function gotMilk(){
+    var milk = 4;
+    var cookies = 1;
     var cupMilk = milk * cookies;
-    console.log(cupMilk);
-    if(milk < 2);
+    if(milk < 2){
+        console.log("You don't have enough milk for your cookies")
+
+    }
+    if(milk >= 2){
+        console.log("Nom nom nom")
+    }
     console.log("I will need " + cupMilk + " ounces of milk for every two cookies");
 }
-gotMilk(2);
+gotMilk();
