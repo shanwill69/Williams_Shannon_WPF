0f376c54364077e1fb787dea38c5014ad55e5616
Shannon <shanwill69@fullsail.com>
2014-07-29 12:57:10 -0500

    worksheet
    practicing functionals

---
+++ Functionals_Worksheet/js/script.js
@@ -0,0 +1,11 @@
+//Shannon Williams July 28, 2014 Functionals Worksheet
+
+function beesKill(){
+    var weight = 150;
+    var stings = 8.66666667;
+    var total = weight * stings;
+    console.log("It will take " + total + " stings to kill this animal");
+}
+
+
+beesKill();
